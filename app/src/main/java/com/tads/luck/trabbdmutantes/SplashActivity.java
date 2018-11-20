8706520b95b738fb8fabae7910f6b6d532b55af2
LUCK-SNOW <lka1994@hotmail.com>
2018-11-20 14:48:26 -0200

    Fix crash varias pesquisas

--- app/src/main/java/com/tads/luck/trabbdmutantes/SplashActivity.java
+++ app/src/main/java/com/tads/luck/trabbdmutantes/SplashActivity.java
@@ -22,6 +22,6 @@ public class SplashActivity extends AppCompatActivity {
                 startActivity(new Intent(getBaseContext(), DashboardActivity.class));
                 finish();
             }
-        },5000);
+        },3000);
     }
 }
